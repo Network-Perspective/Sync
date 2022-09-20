b8ae52394dc280927702addf9d10f5402cba1901
Maciej Klimczuk <maciej@networkperspective.io>
2022-09-20 21:07:33 +0200

    NP-566 Setup Slack integration tests.

---
+++ src/NetworkPerspective.Sync.Common.Tests/Fixtures/InMemoryHostedServiceFixture.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+
+using Moq;
+
+using NetworkPerspective.Sync.Application.Infrastructure.Core;
+using NetworkPerspective.Sync.Application.Infrastructure.Persistence;
+using NetworkPerspective.Sync.Application.Infrastructure.SecretStorage;
+
+namespace NetworkPerspective.Sync.Common.Tests.Fixtures
+{
+    public class InMemoryHostedServiceFixture<TStartup> : WebApplicationFactory<TStartup>
+        where TStartup : class
+    {
+        public SqliteUnitOfWorkFactory UnitOfWorkFactory { get; } = new SqliteUnitOfWorkFactory();
+
+        public Mock<INetworkPerspectiveCore> NetworkPerspectiveCoreMock = new Mock<INetworkPerspectiveCore>();
+        public Mock<ISecretRepositoryFactory> SecretRepositoryFactoryMock = new Mock<ISecretRepositoryFactory>();
+        public Mock<ISecretRepository> SecretRepositoryMock = new Mock<ISecretRepository>();
+
+        public string ValidToken = "valid-token";
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton<IUnitOfWorkFactory>(UnitOfWorkFactory);
+                services.AddSingleton(Mock.Of<IDbInitializer>());
+
+                services.AddSingleton(NetworkPerspectiveCoreMock.Object);
+                services.AddSingleton(SecretRepositoryFactoryMock.Object);
+            });
+        }
+
+        public void Reset()
+        {
+            NetworkPerspectiveCoreMock.Reset();
+            SecretRepositoryFactoryMock.Reset();
+            SecretRepositoryMock.Reset();
+
+            SecretRepositoryFactoryMock
+                .Setup(x => x.CreateAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(SecretRepositoryMock.Object);
+        }
+
+        protected override void ConfigureClient(HttpClient client)
+        {
+            base.ConfigureClient(client);
+
+            client.DefaultRequestHeaders.Add(HeaderNames.Authorization, $"Bearer {ValidToken}");
+        }
+    }
+}
---
+++ src/NetworkPerspective.Sync.Slack.Tests/BaseTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Security;
+using System.Threading;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using NetworkPerspective.Sync.Application.Domain;
+using NetworkPerspective.Sync.Application.Extensions;
+using NetworkPerspective.Sync.Application.Infrastructure.Core.Exceptions;
+using NetworkPerspective.Sync.Common.Tests.Fixtures;
+using NetworkPerspective.Sync.Infrastructure.Slack;
+using NetworkPerspective.Sync.Slack.Client;
+
+using Xunit;
+
+namespace NetworkPerspective.Sync.Slack.Tests
+{
+    public class BaseTest : IClassFixture<InMemoryHostedServiceFixture<Startup>>
+    {
+        private readonly InMemoryHostedServiceFixture<Startup> _service;
+
+        public BaseTest(InMemoryHostedServiceFixture<Startup> service)
+        {
+            _service = service;
+            service.Reset();
+        }
+
+        [Fact]
+        public async Task ShouldSetupNetworkProperly()
+        {
+            // Arrange
+            var networkId = Guid.NewGuid();
+            var httpClient = _service.CreateDefaultClient();
+            var client = new NetworksClient(httpClient);
+
+            const bool autoJoinChannels = true;
+            const bool syncChannelsNames = true;
+
+            _service.NetworkPerspectiveCoreMock
+                .Setup(x => x.ValidateTokenAsync(It.IsAny<SecureString>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new TokenValidationResponse(networkId, Guid.NewGuid()));
+
+            // Act
+            var result = await client.NetworksPostAsync(autoJoinChannels, syncChannelsNames, null);
+
+            // Assert
+            _service.SecretRepositoryMock.Verify(x => x.SetSecretAsync($"np-token-Slack-{networkId}", It.Is<SecureString>(x => x.ToSystemString() == _service.ValidToken), It.IsAny<CancellationToken>()), Times.Once);
+
+            using var unitOfWork = _service.UnitOfWorkFactory.Create();
+            var networksRepository = unitOfWork.GetNetworkRepository<SlackNetworkProperties>();
+            var network = await networksRepository.FindAsync(networkId);
+            network.Properties.AutoJoinChannels.Should().Be(autoJoinChannels);
+            network.Properties.SyncGroups.Should().Be(syncChannelsNames);
+            network.Properties.ExternalKeyVaultUri.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task ShouldReturnError()
+        {
+            // Arrange
+            var httpClient = _service.CreateDefaultClient();
+            var client = new NetworksClient(httpClient);
+
+            _service.NetworkPerspectiveCoreMock
+                .Setup(x => x.ValidateTokenAsync(It.IsAny<SecureString>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new InvalidTokenException("https://networkperspective.io/"));
+
+            // Act
+            var exception = await Record.ExceptionAsync(() => client.NetworksPostAsync(true, true, null));
+
+            // Assert
+            (exception as SlackClientException).StatusCode.Should().Be(StatusCodes.Status401Unauthorized);
+        }
+    }
+}
