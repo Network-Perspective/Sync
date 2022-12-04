2f86363705b4ab2003d363cdea7a03ee3d9ba1b3
Maciej Klimczuk <maciej@networkperspective.io>
2022-12-04 20:07:59 +0100

    NP-783 Add alive endpoint.

---
+++ src/NetworkPerspective.Sync.GSuite.Tests/Fixtures/GSuiteTestsCollection.cs
@@ -0,0 +1,12 @@
+﻿using NetworkPerspective.Sync.Common.Tests.Fixtures;
+
+using Xunit;
+
+namespace NetworkPerspective.Sync.GSuite.Tests.Fixtures
+{
+    [CollectionDefinition(Name)]
+    public class GSuiteTestsCollection : ICollectionFixture<InMemoryHostedServiceFixture<Startup>>
+    {
+        public const string Name = "GSuiteTestsCollection";
+    }
+}
