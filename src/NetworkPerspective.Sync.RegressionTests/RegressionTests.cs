0d44fd20452838425ef5cfe94c1a93aa75521dcb
Maciej Klimczuk <maciej@networkperspective.io>
2023-01-14 17:34:33 +0100

    NP-799 Minor adjustment.

--- src/NetworkPerspective.Sync.RegressionTests/RegressionTests.cs
+++ src/NetworkPerspective.Sync.RegressionTests/RegressionTests.cs
@@ -17,8 +17,8 @@ namespace NetworkPerspective.Sync.RegressionTests
         public async Task Should()
         {
             // Arrange
-            const string oldInteractionsDirPath = "D:\\dane\\old\\interactions";
-            const string newInteractionsDirPath = "D:\\dane\\new\\interactions";
+            const string oldInteractionsDirPath = "D:\\data\\old\\interactions";
+            const string newInteractionsDirPath = "D:\\data\\new\\interactions";
 
             var oldInteractions = await new InteractionsFromFileProvider(oldInteractionsDirPath).GetInteractionsAsync();
             var newInteractions = await new InteractionsFromFileProvider(newInteractionsDirPath).GetInteractionsAsync();
@@ -28,7 +28,6 @@ namespace NetworkPerspective.Sync.RegressionTests
             var comparer = new Comparer<HashedInteraction>(equalityComparer);
 
             var newDist = newInteractions.Distinct(equalityComparer).ToList();
-
             var overlaping = newInteractions.Except(newDist).ToList();
 
             // Act
