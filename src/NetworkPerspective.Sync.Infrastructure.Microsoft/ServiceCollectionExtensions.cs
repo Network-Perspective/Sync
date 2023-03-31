186fed9f9b5a8a770c50ba12e0838205f566fdc7
Maciej Klimczuk <maciej@networkperspective.io>
2023-03-31 14:36:02 +0200

    NP-896 Add resiliency.

--- src/NetworkPerspective.Sync.Infrastructure.Microsoft/ServiceCollectionExtensions.cs
+++ src/NetworkPerspective.Sync.Infrastructure.Microsoft/ServiceCollectionExtensions.cs
@@ -2,14 +2,17 @@
 using Microsoft.Extensions.DependencyInjection;
 
 using NetworkPerspective.Sync.Application.Infrastructure.DataSources;
+using NetworkPerspective.Sync.Infrastructure.Microsoft.Configs;
 using NetworkPerspective.Sync.Infrastructure.Microsoft.Services;
 
 namespace NetworkPerspective.Sync.Infrastructure.Microsoft
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddMicrosoft(this IServiceCollection services, IConfigurationSection configSection)
+        public static IServiceCollection AddMicrosoft(this IServiceCollection services, IConfigurationSection configurationSection)
         {
+            services.Configure<Resiliency>(configurationSection.GetSection("Resiliency"));
+
             services.AddTransient<IMicrosoftClientFactory, MicrosoftClientFactory>();
             services.AddSingleton<IDataSourceFactory, MicrosoftFacadeFactory>();
             return services;
--- src/NetworkPerspective.Sync.Infrastructure.Microsoft/Services/IMicrosoftClientFactory.cs
+++ src/NetworkPerspective.Sync.Infrastructure.Microsoft/Services/IMicrosoftClientFactory.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
 using Azure.Identity;
 
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.Graph;
+using Microsoft.Kiota.Authentication.Azure;
 
 using NetworkPerspective.Sync.Application.Extensions;
 using NetworkPerspective.Sync.Application.Infrastructure.SecretStorage;
+using NetworkPerspective.Sync.Infrastructure.Microsoft.Configs;
+
+using Polly;
+using Polly.Extensions.Http;
 
 namespace NetworkPerspective.Sync.Infrastructure.Microsoft.Services
 {
@@ -16,18 +25,37 @@ namespace NetworkPerspective.Sync.Infrastructure.Microsoft.Services
         Task<GraphServiceClient> GetMicrosoftClientAsync(Guid networkId, CancellationToken stoppingToken = default);
     }
 
-    public class MicrosoftClientFactory : IMicrosoftClientFactory
+    internal class MicrosoftClientFactory : IMicrosoftClientFactory
     {
-        private readonly string[] Scopes = new[] { "https://graph.microsoft.com/.default" };
-
         private readonly ISecretRepositoryFactory _secretRepositoryFactory;
-
-        public MicrosoftClientFactory(ISecretRepositoryFactory secretRepositoryFactory)
+        private readonly PolicyHttpMessageHandler _retryHandler;
+        public MicrosoftClientFactory(ISecretRepositoryFactory secretRepositoryFactory, IOptions<Resiliency> resiliencyOptions, ILoggerFactory loggerFactory)
         {
             _secretRepositoryFactory = secretRepositoryFactory;
+
+            var retryLogger = loggerFactory.CreateLogger<GraphServiceClient>();
+
+            void OnRetry(DelegateResult<HttpResponseMessage> result, TimeSpan timespan)
+            {
+                retryLogger.LogInformation(result.Exception, "Problem occured on calling graph api. Next attempt in {timespan}", timespan);
+            };
+
+            var policy = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(resiliencyOptions.Value.Retries, OnRetry);
+
+            _retryHandler = new PolicyHttpMessageHandler(policy);
         }
 
         public async Task<GraphServiceClient> GetMicrosoftClientAsync(Guid networkId, CancellationToken stoppingToken = default)
+        {
+            var authProvider = await BuildAuthProvider(networkId, stoppingToken);
+            var httpClient = BuildHttpClient();
+
+            return new GraphServiceClient(httpClient, authProvider);
+        }
+
+        private async Task<AzureIdentityAuthenticationProvider> BuildAuthProvider(Guid networkId, CancellationToken stoppingToken)
         {
             var secretRepository = await _secretRepositoryFactory.CreateAsync(networkId, stoppingToken);
 
@@ -46,8 +74,14 @@ namespace NetworkPerspective.Sync.Infrastructure.Microsoft.Services
             };
 
             var clientSecretCredential = new ClientSecretCredential(tenantId.ToSystemString(), clientId.ToSystemString(), clientSecret.ToSystemString(), options);
+            return new AzureIdentityAuthenticationProvider(clientSecretCredential, Array.Empty<string>());
+        }
 
-            return new GraphServiceClient(clientSecretCredential, Scopes);
+        private HttpClient BuildHttpClient()
+        {
+            var handlers = GraphClientFactory.CreateDefaultHandlers();
+            handlers.Add(_retryHandler);
+            return GraphClientFactory.Create(handlers);
         }
     }
 }
