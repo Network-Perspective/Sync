037dcf8f14ae9e7cc66ea932197415d45b56dbd5
Maciej Klimczuk <maciej@networkperspective.io>
2024-02-25 20:53:59 +0100

    NP-1036

--- src/Excel/NetworkPerspective.Sync.Excel/Controllers/SyncController.cs
+++ src/Excel/NetworkPerspective.Sync.Excel/Controllers/SyncController.cs
@@ -2,7 +2,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using System.Threading.Tasks;
 
-
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
