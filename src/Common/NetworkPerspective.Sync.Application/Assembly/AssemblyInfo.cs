6601f4fb10642b34c2eb312ec7ae5d5fe544a62a
Maciej Klimczuk <maciej@networkperspective.io>
2024-05-05 10:58:08 +0200

    Clean up.

--- src/Common/NetworkPerspective.Sync.Application/Assembly/AssemblyInfo.cs
+++ src/Common/NetworkPerspective.Sync.Application/Assembly/AssemblyInfo.cs
@@ -1,6 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NetworkPerspective.Sync.Application.Tests")]
-[assembly: InternalsVisibleTo("NetworkPerspective.Sync.Connector.Application")]
-[assembly: InternalsVisibleTo("NetworkPerspective.Sync.Orchestrator.Application")]
-[assembly: InternalsVisibleTo("NetworkPerspective.Sync.Orchestrator")]
--- src/Common/NetworkPerspective.Sync.Scheduler/Assembly/AssemblyInfo.cs
+++ src/Common/NetworkPerspective.Sync.Scheduler/Assembly/AssemblyInfo.cs
@@ -1,4 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NetworkPerspective.Sync.Scheduler.Tests")]
-[assembly: InternalsVisibleTo("NetworkPerspective.Sync.Orchestrator.Application")]
