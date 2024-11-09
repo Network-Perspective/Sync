d90a667c51603621eed5c310219708aaecca01ae
Maciej Klimczuk <maciej@networkperspective.io>
2024-11-09 19:21:27 +0100

    NP-1244 Auth tester for Microsoft implementation.

--- src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Microsoft/Services/AuthTester.cs
+++ src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Microsoft/Services/AuthTester.cs
@@ -1,17 +1,28 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
 
 using NetworkPerspective.Sync.Worker.Application.Services;
 
 namespace NetworkPerspective.Sync.Infrastructure.DataSources.Microsoft.Services;
 
-internal class AuthTester(ILogger<AuthTester> logger) : IAuthTester
+internal class AuthTester(IConnectorInfoProvider connectorInfoProvider, GraphServiceClient graphClient, ILogger<AuthTester> logger) : IAuthTester
 {
-    public Task<bool> IsAuthorizedAsync(CancellationToken stoppingToken = default)
+    public async Task<bool> IsAuthorizedAsync(CancellationToken stoppingToken = default)
     {
-        logger.LogWarning($"{nameof(AuthTester)} for Microsoft has no implementation yet. Returning true, but it's not a real check.");
-        return Task.FromResult(true);
+        var connectorInfo = connectorInfoProvider.Get();
+        try
+        {
+            var me = await graphClient.Users.GetAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            logger.LogInformation(ex, "Connector '{connectorId}' is not authorized", connectorInfo.Id);
+            return false;
+        }
     }
 }
