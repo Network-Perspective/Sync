b0ff8943287980151cc57ce143d35e7a1c0c3015
Maciej Klimczuk <maciej@networkperspective.io>
2024-11-09 18:25:12 +0100

    NP-1228 Require capabilities to assume worker is capable for something.

--- src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Excel/ServiceCollectionExtensions.cs
+++ src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Excel/ServiceCollectionExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 using NetworkPerspective.Sync.Worker.Application.Domain.Connectors;
 using NetworkPerspective.Sync.Worker.Application.Infrastructure.DataSources;
+using NetworkPerspective.Sync.Worker.Application.Services;
 
 namespace NetworkPerspective.Sync.Infrastructure.DataSources.Excel;
 
@@ -11,6 +13,12 @@ public static class ServiceCollectionExtensions
     private const string SyncConstraintsConfigSection = "SyncConstraints";
     public static IServiceCollection AddExcel(this IServiceCollection services, IConfigurationSection config, ConnectorType connectorType)
     {
+        services.AddTransient<ICapabilityTester>(x =>
+        {
+            var logger = x.GetRequiredService<ILogger<CapabilityTester>>();
+            return new CapabilityTester(connectorType, logger);
+        });
+
         services.AddKeyedScoped<IDataSource, ExcelFacade>(connectorType.GetKeyOf<IDataSource>());
 
         services.Configure<ExcelSyncConstraints>(config.GetSection(SyncConstraintsConfigSection));
---
+++ src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Jira/Services/CapabilityTester.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using NetworkPerspective.Sync.Infrastructure.DataSources.Jira.Client;
+using NetworkPerspective.Sync.Infrastructure.Vaults.Contract;
+using NetworkPerspective.Sync.Infrastructure.Vaults.Contract.Extensions;
+using NetworkPerspective.Sync.Worker.Application.Domain.Connectors;
+
+using NetworkPerspective.Sync.Worker.Application.Services;
+
+namespace NetworkPerspective.Sync.Infrastructure.DataSources.Jira.Services
+{
+    internal class CapabilityTester(ConnectorType connectorType, IVault vault, ILogger<CapabilityTester> logger) : ICapabilityTester
+    {
+        public async Task<IEnumerable<ConnectorType>> GetCapabilitiesAsync(CancellationToken stoppingToken = default)
+        {
+            var areClientSecretsSet = await vault.CanGetSecretsAsync(stoppingToken, JiraClientKeys.JiraClientIdKey, JiraClientKeys.JiraClientSecretKey);
+            logger.LogDebug("Client Id and Secret are accessible");
+
+            if (areClientSecretsSet)
+                return [connectorType];
+            else
+                return [];
+        }
+    }
+}
