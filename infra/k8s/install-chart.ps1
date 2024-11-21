2a751e1472d0de04a8940893dc38977d81e62ed8
Błażej Żak <blazej@networkperspective.io>
2024-11-21 18:31:17 +0100

    NP-1247 update readmes and helm install scripts

--- infra/k8s/install-chart.ps1
+++ infra/k8s/install-chart.ps1
@@ -1,2 +1,2 @@
-# using local aws values
-helm upgrade --install np-worker oci://networkperspective.azurecr.io/helm/np-worker -f ../aws/values-tilt.yaml
+# using local aws values, customize with your own values
+helm upgrade --install np-worker oci://networkperspective.azurecr.io/helm/np-worker -f ../aws/values-tilt.yaml 
