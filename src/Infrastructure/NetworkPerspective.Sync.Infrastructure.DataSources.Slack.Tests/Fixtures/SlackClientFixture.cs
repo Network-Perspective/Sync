dbb1192a5e3791806bbe8c0468d8b09b2adfafbf
Maciej Klimczuk <maciej@networkperspective.io>
2024-11-22 15:33:06 +0100

    NP-1259 Improve GetStatus functionality.

--- src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Slack.Tests/Fixtures/SlackClientFixture.cs
+++ src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Slack.Tests/Fixtures/SlackClientFixture.cs
@@ -10,42 +10,41 @@ using NetworkPerspective.Sync.Infrastructure.DataSources.Slack.Client.Configs;
 using NetworkPerspective.Sync.Infrastructure.DataSources.Slack.Client.HttpClients;
 using NetworkPerspective.Sync.Infrastructure.Vaults.AzureKeyVault;
 
-namespace NetworkPerspective.Sync.Infrastructure.DataSources.Slack.Tests.Fixtures
+namespace NetworkPerspective.Sync.Infrastructure.DataSources.Slack.Tests.Fixtures;
+
+public class SlackClientFixture : IDisposable
 {
-    public class SlackClientFixture : IDisposable
+    public ISlackHttpClient SlackHttpClient { get; }
+
+    public SlackClientFixture()
     {
-        public ISlackHttpClient SlackHttpClient { get; }
+        var secretStorageOptions = Options.Create(new AzureKeyVaultConfig { BaseUrl = "https://nptestvault.vault.azure.net/" });
 
-        public SlackClientFixture()
+        var secretRepository = new AzureKeyVaultClient(TokenCredentialFactory.Create(), secretStorageOptions, NullLogger<AzureKeyVaultClient>.Instance);
+        var slackToken = secretRepository.GetSecretAsync(string.Format(SlackKeys.BotTokenKeyPattern, "unit-test")).Result;
+
+        var httpClient = new HttpClient
         {
-            var secretStorageOptions = Options.Create(new AzureKeyVaultConfig { BaseUrl = "https://nptestvault.vault.azure.net/" });
+            BaseAddress = new Uri("https://slack.com/api/")
+        };
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", new NetworkCredential(string.Empty, slackToken).Password);
 
-            var secretRepository = new AzureKeyVaultClient(TokenCredentialFactory.Create(), secretStorageOptions, NullLogger<AzureKeyVaultClient>.Instance);
-            var slackToken = secretRepository.GetSecretAsync(string.Format(SlackKeys.TokenKeyPattern, "unit-test")).Result;
-
-            var httpClient = new HttpClient
+        var resiliency = new Resiliency
+        {
+            Retries = new[]
             {
-                BaseAddress = new Uri("https://slack.com/api/")
-            };
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", new NetworkCredential(string.Empty, slackToken).Password);
-
-            var resiliency = new Resiliency
-            {
-                Retries = new[]
-                {
-                    TimeSpan.FromMilliseconds(100),
-                    TimeSpan.FromMilliseconds(300),
-                    TimeSpan.FromMilliseconds(500),
-                    TimeSpan.FromMilliseconds(1000)
-                }
-            };
-            SlackHttpClient = new ResilientSlackHttpClientDecorator(
-                new SlackHttpClient(httpClient, NullLogger<SlackHttpClient>.Instance),
-                resiliency,
-                NullLogger<ResilientSlackHttpClientDecorator>.Instance);
-        }
-
-        public void Dispose()
-            => SlackHttpClient?.Dispose();
+                TimeSpan.FromMilliseconds(100),
+                TimeSpan.FromMilliseconds(300),
+                TimeSpan.FromMilliseconds(500),
+                TimeSpan.FromMilliseconds(1000)
+            }
+        };
+        SlackHttpClient = new ResilientSlackHttpClientDecorator(
+            new SlackHttpClient(httpClient, NullLogger<SlackHttpClient>.Instance),
+            resiliency,
+            NullLogger<ResilientSlackHttpClientDecorator>.Instance);
     }
+
+    public void Dispose()
+        => SlackHttpClient?.Dispose();
 }
