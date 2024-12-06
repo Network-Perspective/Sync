245cf567a7682ec4808e0d500949b28e960aac38
Błażej Żak <blazej@networkperspective.io>
2024-12-06 15:34:44 +0100

    NP-1270 log worker version on startup

---
+++ src/SingleContainer/Worker/NetworkPerspective.Sync.Worker/ApplicationInsights/DiagnosticExtensions.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace NetworkPerspective.Sync.Worker.ApplicationInsights;
+
+public static class DiagnosticExtensions
+{
+    public static IServiceCollection LogApplicationVersion(this IServiceCollection services)
+    {
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
+        var logger = services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+        logger.LogInformation("Application starting. Version: {Version}", version);
+        
+        return services;
+    }
+}
