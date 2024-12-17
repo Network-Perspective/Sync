6b9ea3bcf90114eb07028b29e67a9052fd8cb740
Maciej Klimczuk <maciej@networkperspective.io>
2024-12-17 20:57:31 +0100

    NP-1260 Configurable scheduler start delay.

--- src/SingleContainer/Orchestrator/NetworkPerspective.Sync.Orchestrator.Application/Scheduler/Sync/ServiceCollectionExtensions.cs
+++ src/SingleContainer/Orchestrator/NetworkPerspective.Sync.Orchestrator.Application/Scheduler/Sync/ServiceCollectionExtensions.cs
@@ -1,57 +1,16 @@
-﻿using System;
-
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-using Quartz;
 
 namespace NetworkPerspective.Sync.Orchestrator.Application.Scheduler.Sync;
 
 internal static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddSyncScheduler(this IServiceCollection services, IConfigurationSection configurationSection, string dbConnectionString)
+    public static IServiceCollection AddSyncScheduler(this IServiceCollection services, IConfiguration configuration)
     {
-        var schedulerConfig = new SyncSchedulerConfig();
-        configurationSection.Bind(schedulerConfig);
-        services.Configure<SyncSchedulerConfig>(configurationSection);
+        services.Configure<SyncSchedulerConfig>(configuration);
 
         services.AddTransient<IJobDetailFactory, JobDetailFactory<RemoteSyncJob>>();
         services.AddTransient<ISyncScheduler, SyncScheduler>();
-
-        services.AddQuartz(q =>
-        {
-            q.SchedulerId = "scheduler-connector";
-
-            if (schedulerConfig.UsePersistentStore)
-            {
-                q.UsePersistentStore(store =>
-                {
-                    store.UseProperties = true;
-                    store.UseNewtonsoftJsonSerializer();
-                    store.UseSqlServer(db =>
-                    {
-                        db.ConnectionString = dbConnectionString;
-                    });
-                });
-            }
-            else
-            {
-                q.UseInMemoryStore();
-            }
-
-            q.InterruptJobsOnShutdown = true;
-
-            q.UseSimpleTypeLoader();
-            q.UseDefaultThreadPool(threadPool =>
-            {
-                threadPool.MaxConcurrency = 4;
-            });
-        });
-        services.AddQuartzHostedService(q =>
-        {
-            q.WaitForJobsToComplete = true;
-            q.StartDelay = TimeSpan.FromSeconds(10);
-        });
 
         return services;
     }
