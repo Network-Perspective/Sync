4c09999436e0b9a71eb27ec7ad7639693dd5ec00
Maciej Klimczuk <maciej@networkperspective.io>
2025-02-16 16:31:47 +0100

    NP-1226 Fix github vulnerabilities alerts.

--- src/SingleContainer/NetworkPerspective.Sync.Utils/Extensions/StringExtensions.cs
+++ src/SingleContainer/NetworkPerspective.Sync.Utils/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Security;
 
 namespace NetworkPerspective.Sync.Utils.Extensions;
@@ -7,4 +8,24 @@ public static class StringExtensions
 {
     public static SecureString ToSecureString(this string str)
         => new NetworkCredential(string.Empty, str).SecurePassword;
+
+    public static string Sanitize(this string str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return str;
+
+        return str
+                .ToStringWithoutControlCharacters()
+                .ToEscapedString();
+    }
+
+    private static string ToStringWithoutControlCharacters(this string str)
+        => str
+            .Replace(Environment.NewLine, string.Empty)
+            .Replace("\n", string.Empty)
+            .Replace("\r", string.Empty)
+            .Replace("\t", string.Empty);
+
+    private static string ToEscapedString(this string str)
+        => SecurityElement.Escape(str);
 }
--- src/SingleContainer/Orchestrator/NetworkPerspective.Sync.Orchestrator.Application/Services/IErrorService.cs
+++ src/SingleContainer/Orchestrator/NetworkPerspective.Sync.Orchestrator.Application/Services/IErrorService.cs
@@ -1,114 +1,87 @@
 ﻿using System;
+using System.Text;
 
 using Microsoft.Extensions.Logging;
 
+using NetworkPerspective.Sync.Infrastructure.Vaults.Contract.Exceptions;
 using NetworkPerspective.Sync.Orchestrator.Application.Domain;
 using NetworkPerspective.Sync.Orchestrator.Application.Exceptions;
+using NetworkPerspective.Sync.Orchestrator.Application.Infrastructure.Persistence.Exceptions;
 using NetworkPerspective.Sync.Utils.Extensions;
 
-namespace NetworkPerspective.Sync.Orchestrator.Application.Services
+namespace NetworkPerspective.Sync.Orchestrator.Application.Services;
+
+public interface IErrorService
 {
-    public interface IErrorService
-    {
-        Error MapToError(Exception exception);
-    }
+    Error MapToError(Exception exception);
+}
 
-    public class ErrorService : IErrorService
-    {
-        private readonly ILogger<ErrorService> _logger;
+public class ErrorService(ILogger<ErrorService> logger) : IErrorService
+{
+    private readonly ILogger<ErrorService> _logger = logger;
 
-        public ErrorService(ILogger<ErrorService> logger)
+    public Error MapToError(Exception exception)
+    {
+        if (exception.IndicatesTaskCanceled())
         {
-            _logger = logger;
+            _logger.LogInformation("Operation cancelled");
+            return new Error(
+                type: Error.Types.Application,
+                title: "Operation Cancelled",
+                details: "The operation has been canceled before completed",
+                statusCode: 400);
         }
 
-        public Error MapToError(Exception exception)
+        _logger.LogError(exception, "Application has thrown an exception");
+
+        switch (exception)
         {
-            if (exception.IndicatesTaskCanceled())
-            {
-                _logger.LogInformation("Operation cancelled");
-                return new Error(
-                    type: Error.Types.Application,
-                    title: "Operation Cancelled",
-                    details: "The operation has been canceled before completed",
-                    statusCode: 400);
-            }
+            case MissingAuthorizationHeaderException mahex:
+                {
+                    return new Error(
+                        type: Error.Types.Security,
+                        title: "Authentication Error",
+                        details: mahex.Message,
+                        statusCode: 401);
+                }
+            case OAuthException aex:
+                {
+                    var details = new StringBuilder();
+                    details.Append(aex.Error);
 
-            _logger.LogError(exception, "Application has thrown an exception");
+                    if (!string.IsNullOrEmpty(aex.ErrorDescription))
+                        details.Append($" ({aex.ErrorDescription})");
 
-            switch (exception)
-            {
-                case MissingAuthorizationHeaderException mahex:
-                    {
-                        return new Error(
-                            type: Error.Types.Security,
-                            title: "Authentication Error",
-                            details: mahex.Message,
-                            statusCode: 401);
-                    }
-                //case InvalidTokenException iatex:
-                //    {
-                //        return new Error(
-                //            type: Error.Types.Security,
-                //            title: "Authentication Error",
-                //            details: iatex.Message,
-                //            statusCode: StatusCodes.Status401Unauthorized);
-                //    }
-                //case OAuthException aex:
-                //    {
-                //        var details = new StringBuilder();
-                //        details.Append(aex.Error);
-
-                //        if (!string.IsNullOrEmpty(aex.ErrorDescription))
-                //            details.Append($" ({aex.ErrorDescription})");
-
-                //        return new Error(
-                //            type: Error.Types.Security,
-                //            title: "OAuth error",
-                //            details: details.ToString(),
-                //            statusCode: StatusCodes.Status400BadRequest);
-                //    }
-                //case SecretStorageException ssex:
-                //    {
-                //        return new Error(
-                //            type: Error.Types.SecretStorage,
-                //            title: "Secret Storage Interface Error",
-                //            details: ssex.Message,
-                //            statusCode: StatusCodes.Status500InternalServerError);
-                //    }
-                //case DbException dbex:
-                //    {
-                //        return new Error(
-                //            type: Error.Types.Database,
-                //            title: "Database Error",
-                //            details: dbex.Message,
-                //            statusCode: StatusCodes.Status500InternalServerError);
-                //    }
-                //case NetworkPerspectiveCoreException npcex:
-                //    {
-                //        return new Error(
-                //            type: Error.Types.NetworkPerspectiveCore,
-                //            title: "Network Perspective Core Interface Error",
-                //            details: npcex.Message,
-                //            statusCode: StatusCodes.Status500InternalServerError);
-                //    }
-                //case NetworkNotFoundException nnfex:
-                //    {
-                //        return new Error(
-                //            type: Error.Types.Application,
-                //            title: "Application Error",
-                //            details: $"Network '{nnfex.NetworkId}' doesn't exist. Please add network",
-                //            statusCode: StatusCodes.Status404NotFound);
-                //    }
-                default:
-                    {
-                        return new Error(
-                            type: Error.Types.Unknown,
-                            title: "Unexpected Error",
-                            details: "Unexpected exception has been thrown. Please see logs for details",
-                            statusCode: 500);
-                    }
-            }
+                    return new Error(
+                        type: Error.Types.Security,
+                        title: "OAuth error",
+                        details: details.ToString(),
+                        statusCode: 400);
+                }
+            case VaultException vex:
+                {
+                    return new Error(
+                        type: Error.Types.SecretStorage,
+                        title: "Vault Interface Error",
+                        details: vex.Message,
+                        statusCode: 500);
+                }
+            case DbException dbex:
+                {
+                    return new Error(
+                        type: Error.Types.Database,
+                        title: "Database Error",
+                        details: dbex.Message,
+                        statusCode: 500);
+                }
+            default:
+                {
+                    return new Error(
+                        type: Error.Types.Unknown,
+                        title: "Unexpected Error",
+                        details: "Unexpected exception has been thrown. Please see logs for details",
+                        statusCode: 500);
+                }
         }
     }
 }
