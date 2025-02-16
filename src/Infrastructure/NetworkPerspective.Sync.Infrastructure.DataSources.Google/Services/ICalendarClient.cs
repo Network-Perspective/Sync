fda1b07937e9abddcc78275e7ba7a88beb9aa808
MaciejK <151932782+MaciejKlimczuk@users.noreply.github.com>
2025-02-16 19:13:32 +0100

    NP-1226 Fix github vulnerabilities alerts. (#105)
    * NP-1226 Fix github vulnerabilities alerts.
    
    * NP-1226 Fix tests.
    
    * NP-1226 Remove not used logging.

--- src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Google/Services/ICalendarClient.cs
+++ src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Google/Services/ICalendarClient.cs
@@ -18,137 +18,119 @@ using NetworkPerspective.Sync.Worker.Application.Domain.Statuses;
 using NetworkPerspective.Sync.Worker.Application.Domain.Sync;
 using NetworkPerspective.Sync.Worker.Application.Services.TasksStatuses;
 
-namespace NetworkPerspective.Sync.Infrastructure.DataSources.Google.Services
+namespace NetworkPerspective.Sync.Infrastructure.DataSources.Google.Services;
+
+internal interface ICalendarClient
 {
-    internal interface ICalendarClient
+    public Task<SyncResult> SyncInteractionsAsync(SyncContext context, IInteractionsStream stream, IEnumerable<string> usersEmails, MeetingInteractionFactory interactionFactory, CancellationToken stoppingToken = default);
+}
+
+internal sealed class CalendarClient(IGlobalStatusCache tasksStatusesCache, IOptions<GoogleConfig> config, IRetryPolicyProvider retryPolicyProvider, IImpesonificationCredentialsProvider credentialsProvider, ILogger<CalendarClient> logger) : ICalendarClient
+{
+    private const string TaskCaption = "Synchronizing callendar interactions";
+    private const string TaskDescription = "Fetching callendar metadata from Google API";
+
+    private readonly GoogleConfig _config = config.Value;
+
+    public async Task<SyncResult> SyncInteractionsAsync(SyncContext context, IInteractionsStream stream, IEnumerable<string> usersEmails, MeetingInteractionFactory interactionFactory, CancellationToken stoppingToken = default)
     {
-        public Task<SyncResult> SyncInteractionsAsync(SyncContext context, IInteractionsStream stream, IEnumerable<string> usersEmails, MeetingInteractionFactory interactionFactory, CancellationToken stoppingToken = default);
+        async Task ReportProgressCallbackAsync(double progressRate)
+        {
+            var taskStatus = SingleTaskStatus.New(TaskCaption, TaskDescription, progressRate);
+            await tasksStatusesCache.SetStatusAsync(context.ConnectorId, taskStatus, stoppingToken);
+        }
+
+        Task<SingleTaskResult> SingleTaskAsync(string userEmail)
+        {
+            var retryPolicy = retryPolicyProvider.GetSecretRotationRetryPolicy();
+            return retryPolicy.ExecuteAsync(() => GetSingleUserInteractionsAsync(context, stream, userEmail, interactionFactory, stoppingToken));
+        }
+
+        logger.LogInformation("Evaluating interactions based on callendar for '{timerange}' for {count} users...", context.TimeRange, usersEmails.Count());
+        var result = await ParallelSyncTask<string>.RunAsync(usersEmails, ReportProgressCallbackAsync, SingleTaskAsync, stoppingToken);
+        logger.LogInformation("Evaluation of interactions based on callendar for '{timerange}' completed", context.TimeRange);
+
+        return result;
     }
 
-    internal sealed class CalendarClient : ICalendarClient
+    private async Task<SingleTaskResult> GetSingleUserInteractionsAsync(SyncContext context, IInteractionsStream stream, string userEmail, MeetingInteractionFactory interactionFactory, CancellationToken stoppingToken)
     {
-        private const string TaskCaption = "Synchronizing callendar interactions";
-        private const string TaskDescription = "Fetching callendar metadata from Google API";
-
-        private readonly GoogleConfig _config;
-        private readonly IGlobalStatusCache _tasksStatusesCache;
-        private readonly IRetryPolicyProvider _retryPolicyProvider;
-        private readonly ILogger<CalendarClient> _logger;
-        private readonly IImpesonificationCredentialsProvider _credentialsProvider;
-
-        public CalendarClient(IGlobalStatusCache tasksStatusesCache, IOptions<GoogleConfig> config, IRetryPolicyProvider retryPolicyProvider, IImpesonificationCredentialsProvider credentialsProvider, ILogger<CalendarClient> logger)
+        try
         {
-            _config = config.Value;
-            _tasksStatusesCache = tasksStatusesCache;
-            _retryPolicyProvider = retryPolicyProvider;
-            _credentialsProvider = credentialsProvider;
-            _logger = logger;
-        }
+            logger.LogDebug("Evaluating interactions based on callendar for user '{email}' for period {timeRange}...", "***", context.TimeRange);
 
-        public async Task<SyncResult> SyncInteractionsAsync(SyncContext context, IInteractionsStream stream, IEnumerable<string> usersEmails, MeetingInteractionFactory interactionFactory, CancellationToken stoppingToken = default)
+            int interactionsCount = 0;
+
+            var credentials = await credentialsProvider.ImpersonificateAsync(userEmail, stoppingToken);
+
+            var calendarService = new CalendarService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credentials,
+                ApplicationName = _config.ApplicationName
+            });
+
+            var request = calendarService.Events.List(userEmail);
+            request.TimeMinDateTimeOffset = context.TimeRange.Start;
+            request.TimeMaxDateTimeOffset = context.TimeRange.End;
+            request.SingleEvents = true;
+
+            var response = await retryPolicyProvider
+                .GetThrottlingRetryPolicy()
+                .ExecuteAsync(request.ExecuteAsync, stoppingToken);
+
+            logger.LogDebug("Found '{count}' events in callendar for user '{email}' for period {timeRange}", response.Items.Count, "***", context.TimeRange);
+
+            foreach (var meeting in response.Items)
+            {
+                var recurrence = await GetRecurrenceAsync(calendarService, userEmail, meeting.RecurringEventId, stoppingToken);
+                var interactions = interactionFactory.CreateForUser(meeting, userEmail, recurrence);
+                var sentInteractionsCount = await stream.SendAsync(interactions);
+                interactionsCount += sentInteractionsCount;
+            }
+
+            logger.LogDebug("Evaluation of interactions based on callendar for user '{email}' completed", "***");
+            return new SingleTaskResult(interactionsCount);
+        }
+        catch (GoogleApiException gaex) when (IndicatesIsNotACalendarUser(gaex))
         {
-            async Task ReportProgressCallbackAsync(double progressRate)
-            {
-                var taskStatus = SingleTaskStatus.New(TaskCaption, TaskDescription, progressRate);
-                await _tasksStatusesCache.SetStatusAsync(context.ConnectorId, taskStatus, stoppingToken);
-            }
-
-            Task<SingleTaskResult> SingleTaskAsync(string userEmail)
-            {
-                var retryPolicy = _retryPolicyProvider.GetSecretRotationRetryPolicy();
-                return retryPolicy.ExecuteAsync(() => GetSingleUserInteractionsAsync(context, stream, userEmail, interactionFactory, stoppingToken));
-            }
-
-            _logger.LogInformation("Evaluating interactions based on callendar for '{timerange}' for {count} users...", context.TimeRange, usersEmails.Count());
-            var result = await ParallelSyncTask<string>.RunAsync(usersEmails, ReportProgressCallbackAsync, SingleTaskAsync, stoppingToken);
-            _logger.LogInformation("Evaluation of interactions based on callendar for '{timerange}' completed", context.TimeRange);
-
-            return result;
+            logger.LogDebug("The user '{email}' is not a calendar user. Skipping meetings interactions for given user", "***");
+            return SingleTaskResult.Empty;
         }
-
-        private async Task<SingleTaskResult> GetSingleUserInteractionsAsync(SyncContext context, IInteractionsStream stream, string userEmail, MeetingInteractionFactory interactionFactory, CancellationToken stoppingToken)
-        {
-            try
-            {
-                _logger.LogDebug("Evaluating interactions based on callendar for user '{email}' for period {timeRange}...", "***", context.TimeRange);
-                _logger.LogTrace("Evaluating interactions based on callendar for user '{email}' for period {timeRange}...", userEmail, context.TimeRange);
-
-                int interactionsCount = 0;
-
-                var credentials = await _credentialsProvider.ImpersonificateAsync(userEmail, stoppingToken);
-
-                var calendarService = new CalendarService(new BaseClientService.Initializer
-                {
-                    HttpClientInitializer = credentials,
-                    ApplicationName = _config.ApplicationName
-                });
-
-                var request = calendarService.Events.List(userEmail);
-                request.TimeMinDateTimeOffset = context.TimeRange.Start;
-                request.TimeMaxDateTimeOffset = context.TimeRange.End;
-                request.SingleEvents = true;
-
-                var response = await _retryPolicyProvider
-                    .GetThrottlingRetryPolicy()
-                    .ExecuteAsync(request.ExecuteAsync, stoppingToken);
-
-                _logger.LogDebug("Found '{count}' events in callendar for user '{email}' for period {timeRange}", response.Items.Count, "***", context.TimeRange);
-                _logger.LogTrace("Found '{count}' events in callendar for user '{email}' for period {timeRange}", response.Items.Count, userEmail, context.TimeRange);
-
-                foreach (var meeting in response.Items)
-                {
-                    var recurrence = await GetRecurrenceAsync(calendarService, userEmail, meeting.RecurringEventId, stoppingToken);
-                    var interactions = interactionFactory.CreateForUser(meeting, userEmail, recurrence);
-                    var sentInteractionsCount = await stream.SendAsync(interactions);
-                    interactionsCount += sentInteractionsCount;
-                }
-
-                _logger.LogDebug("Evaluation of interactions based on callendar for user '{email}' completed", "***");
-                _logger.LogTrace("Evaluation of interactions based on callendar for user '{email}' completed", userEmail);
-                return new SingleTaskResult(interactionsCount);
-            }
-            catch (GoogleApiException gaex) when (IndicatesIsNotACalendarUser(gaex))
-            {
-                _logger.LogDebug("The user '{email}' is not a calendar user. Skipping meetings interactions for given user", "***");
-                _logger.LogTrace("The user '{email}' is not a calendar user. Skipping meetings interactions for given user", userEmail);
-                return SingleTaskResult.Empty;
-            }
-        }
-
-        private async Task<RecurrenceType?> GetRecurrenceAsync(CalendarService calendarService, string userEmail, string recurrenceEventId, CancellationToken stoppingToken)
-        {
-            if (string.IsNullOrEmpty(recurrenceEventId))
-                return null;
-
-            try
-            {
-                var request = calendarService.Events.Get(userEmail, recurrenceEventId);
-
-                var response = await _retryPolicyProvider
-                    .GetThrottlingRetryPolicy()
-                    .ExecuteAsync(request.ExecuteAsync, stoppingToken);
-                return response.GetRecurrence();
-
-            }
-            catch (GoogleApiException ex) when (IndicatesNotFound(ex))
-            {
-                _logger.LogWarning(ex, "Unable to evaluate interaction's recurrence type - it might be caused by event changed. Assigning default value (null)");
-                return null;
-            }
-
-        }
-
-        private static bool IndicatesIsNotACalendarUser(GoogleApiException ex)
-        {
-            if (ex.Error.Errors.Count != 1)
-                return false;
-
-            var notCalendarUserError = ex.Error.Errors.SingleOrDefault(x => x.Domain == "calendar" && x.Reason == "notACalendarUser");
-
-            return notCalendarUserError is not null;
-        }
-
-        private static bool IndicatesNotFound(GoogleApiException ex)
-            => ex.HttpStatusCode == HttpStatusCode.NotFound;
     }
+
+    private async Task<RecurrenceType?> GetRecurrenceAsync(CalendarService calendarService, string userEmail, string recurrenceEventId, CancellationToken stoppingToken)
+    {
+        if (string.IsNullOrEmpty(recurrenceEventId))
+            return null;
+
+        try
+        {
+            var request = calendarService.Events.Get(userEmail, recurrenceEventId);
+
+            var response = await retryPolicyProvider
+                .GetThrottlingRetryPolicy()
+                .ExecuteAsync(request.ExecuteAsync, stoppingToken);
+            return response.GetRecurrence();
+
+        }
+        catch (GoogleApiException ex) when (IndicatesNotFound(ex))
+        {
+            logger.LogWarning(ex, "Unable to evaluate interaction's recurrence type - it might be caused by event changed. Assigning default value (null)");
+            return null;
+        }
+
+    }
+
+    private static bool IndicatesIsNotACalendarUser(GoogleApiException ex)
+    {
+        if (ex.Error.Errors.Count != 1)
+            return false;
+
+        var notCalendarUserError = ex.Error.Errors.SingleOrDefault(x => x.Domain == "calendar" && x.Reason == "notACalendarUser");
+
+        return notCalendarUserError is not null;
+    }
+
+    private static bool IndicatesNotFound(GoogleApiException ex)
+        => ex.HttpStatusCode == HttpStatusCode.NotFound;
 }
--- src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Google/Services/IMailboxClient.cs
+++ src/Infrastructure/NetworkPerspective.Sync.Infrastructure.DataSources.Google/Services/IMailboxClient.cs
@@ -20,133 +20,111 @@ using NetworkPerspective.Sync.Worker.Application.Extensions;
 using NetworkPerspective.Sync.Worker.Application.Services;
 using NetworkPerspective.Sync.Worker.Application.Services.TasksStatuses;
 
-namespace NetworkPerspective.Sync.Infrastructure.DataSources.Google.Services
+namespace NetworkPerspective.Sync.Infrastructure.DataSources.Google.Services;
+
+internal interface IMailboxClient
 {
-    internal interface IMailboxClient
+    Task<SyncResult> SyncInteractionsAsync(SyncContext context, IInteractionsStream stream, IEnumerable<string> usersEmails, EmailInteractionFactory interactionFactory, CancellationToken stoppingToken = default);
+}
+
+internal sealed class MailboxClient(IGlobalStatusCache tasksStatusesCache, IOptions<GoogleConfig> config, IImpesonificationCredentialsProvider credentialsProvider, IRetryPolicyProvider retryPolicyProvider, IStatusLogger statusLogger, ILoggerFactory loggerFactory, IClock clock) : IMailboxClient
+{
+    private static readonly int MinutesInDay = 24 * 60;
+    private const string TaskCaption = "Synchronizing email interactions";
+    private const string TaskDescription = "Fetching emails metadata from Google API";
+
+    private readonly GoogleConfig _config = config.Value;
+    private readonly ILogger<MailboxClient> _logger = loggerFactory.CreateLogger<MailboxClient>();
+
+    public async Task<SyncResult> SyncInteractionsAsync(SyncContext context, IInteractionsStream stream, IEnumerable<string> usersEmails, EmailInteractionFactory interactionFactory, CancellationToken stoppingToken = default)
     {
-        Task<SyncResult> SyncInteractionsAsync(SyncContext context, IInteractionsStream stream, IEnumerable<string> usersEmails, EmailInteractionFactory interactionFactory, CancellationToken stoppingToken = default);
+        var maxMessagesCountPerUser = CalculateMaxMessagesCount(context.TimeRange);
+
+        async Task ReportProgressCallbackAsync(double progressRate)
+        {
+            var taskStatus = SingleTaskStatus.New(TaskCaption, TaskDescription, progressRate);
+            await tasksStatusesCache.SetStatusAsync(context.ConnectorId, taskStatus, stoppingToken);
+        }
+
+        Task<SingleTaskResult> SingleTaskAsync(string userEmail)
+        {
+            var retryPolicy = retryPolicyProvider.GetSecretRotationRetryPolicy();
+            return retryPolicy.ExecuteAsync(() => GetSingleUserInteractionsAsync(context, stream, userEmail, maxMessagesCountPerUser, interactionFactory, stoppingToken));
+        }
+
+        _logger.LogInformation("Evaluating interactions based on mailbox for timerange {timerange} for {count} users...", context.TimeRange, usersEmails.Count());
+        var result = await ParallelSyncTask<string>.RunAsync(usersEmails, ReportProgressCallbackAsync, SingleTaskAsync, stoppingToken);
+        _logger.LogInformation("Evaluation of interactions based on mailbox for timerange '{timerange}' completed", context.TimeRange);
+
+        return result;
     }
 
-    internal sealed class MailboxClient : IMailboxClient
+    private async Task<SingleTaskResult> GetSingleUserInteractionsAsync(SyncContext context, IInteractionsStream stream, string userEmail, int maxMessagesCount, EmailInteractionFactory interactionFactory, CancellationToken stoppingToken)
     {
-        private static readonly int MinutesInDay = 24 * 60;
-        private const string TaskCaption = "Synchronizing email interactions";
-        private const string TaskDescription = "Fetching emails metadata from Google API";
-
-        private readonly GoogleConfig _config;
-        private readonly ILogger<MailboxClient> _logger;
-        private readonly IGlobalStatusCache _tasksStatusesCache;
-        private readonly IImpesonificationCredentialsProvider _credentialsProvider;
-        private readonly IRetryPolicyProvider _retryPolicyProvider;
-        private readonly IStatusLogger _statusLogger;
-        private readonly ILoggerFactory _loggerFactory;
-        private readonly IClock _clock;
-
-        public MailboxClient(IGlobalStatusCache tasksStatusesCache, IOptions<GoogleConfig> config, IImpesonificationCredentialsProvider credentialsProvider, IRetryPolicyProvider retryPolicyProvider, IStatusLogger statusLogger, ILoggerFactory loggerFactory, IClock clock)
+        try
         {
-            _config = config.Value;
-            _logger = loggerFactory.CreateLogger<MailboxClient>();
-            _tasksStatusesCache = tasksStatusesCache;
-            _credentialsProvider = credentialsProvider;
-            _retryPolicyProvider = retryPolicyProvider;
-            _statusLogger = statusLogger;
-            _loggerFactory = loggerFactory;
-            _clock = clock;
-        }
+            var interactionsCount = 0;
+            _logger.LogDebug("Evaluating interactions based on mailbox for user ***...");
 
-        public async Task<SyncResult> SyncInteractionsAsync(SyncContext context, IInteractionsStream stream, IEnumerable<string> usersEmails, EmailInteractionFactory interactionFactory, CancellationToken stoppingToken = default)
-        {
-            var maxMessagesCountPerUser = CalculateMaxMessagesCount(context.TimeRange);
+            using var gmailService = await InitializeGmailServiceAsync(userEmail, stoppingToken);
+            var mailboxTraverser = new MailboxTraverser(userEmail, maxMessagesCount, gmailService, retryPolicyProvider, loggerFactory.CreateLogger<MailboxTraverser>());
+            var message = await mailboxTraverser.GetNextMessageAsync(stoppingToken);
 
-            async Task ReportProgressCallbackAsync(double progressRate)
+            var periodStart = context.TimeRange.Start.AddMinutes(-_config.SyncOverlapInMinutes);
+            _logger.LogDebug("To not miss any email interactions period start is extended by {minutes}min. As result mailbox interactions are eveluated starting from {start}", _config.SyncOverlapInMinutes, periodStart);
+
+            while (!stoppingToken.IsCancellationRequested && message != null && message?.GetDateTime(clock) > periodStart)
             {
-                var taskStatus = SingleTaskStatus.New(TaskCaption, TaskDescription, progressRate);
-                await _tasksStatusesCache.SetStatusAsync(context.ConnectorId, taskStatus, stoppingToken);
+                var interactions = interactionFactory.CreateForUser(message, userEmail);
+                var sentInteractionsCount = await stream.SendAsync(interactions);
+                interactionsCount += sentInteractionsCount;
+                message = await mailboxTraverser.GetNextMessageAsync(stoppingToken);
             }
 
-            Task<SingleTaskResult> SingleTaskAsync(string userEmail)
-            {
-                var retryPolicy = _retryPolicyProvider.GetSecretRotationRetryPolicy();
-                return retryPolicy.ExecuteAsync(() => GetSingleUserInteractionsAsync(context, stream, userEmail, maxMessagesCountPerUser, interactionFactory, stoppingToken));
-            }
+            _logger.LogDebug("Evaluation interactions based on mailbox for user '{user}' completed. Processed {mailsCount} email/s", "***", mailboxTraverser.FetchedMessagesCount);
 
-            _logger.LogInformation("Evaluating interactions based on mailbox for timerange {timerange} for {count} users...", context.TimeRange, usersEmails.Count());
-            var result = await ParallelSyncTask<string>.RunAsync(usersEmails, ReportProgressCallbackAsync, SingleTaskAsync, stoppingToken);
-            _logger.LogInformation("Evaluation of interactions based on mailbox for timerange '{timerange}' completed", context.TimeRange);
-
-            return result;
+            return new SingleTaskResult(interactionsCount);
         }
-
-        private async Task<SingleTaskResult> GetSingleUserInteractionsAsync(SyncContext context, IInteractionsStream stream, string userEmail, int maxMessagesCount, EmailInteractionFactory interactionFactory, CancellationToken stoppingToken)
+        catch (TooManyMailsPerUserException)
         {
-            try
-            {
-                var interactionsCount = 0;
-                _logger.LogDebug("Evaluating interactions based on mailbox for user ***...");
-
-                using var gmailService = await InitializeGmailServiceAsync(userEmail, stoppingToken);
-                var mailboxTraverser = new MailboxTraverser(userEmail, maxMessagesCount, gmailService, _retryPolicyProvider, _loggerFactory.CreateLogger<MailboxTraverser>());
-                var message = await mailboxTraverser.GetNextMessageAsync(stoppingToken);
-
-                var periodStart = context.TimeRange.Start.AddMinutes(-_config.SyncOverlapInMinutes);
-                _logger.LogDebug("To not miss any email interactions period start is extended by {minutes}min. As result mailbox interactions are eveluated starting from {start}", _config.SyncOverlapInMinutes, periodStart);
-
-                while (!stoppingToken.IsCancellationRequested && message != null && message?.GetDateTime(_clock) > periodStart)
-                {
-                    var interactions = interactionFactory.CreateForUser(message, userEmail);
-                    var sentInteractionsCount = await stream.SendAsync(interactions);
-                    interactionsCount += sentInteractionsCount;
-                    message = await mailboxTraverser.GetNextMessageAsync(stoppingToken);
-                }
-
-                _logger.LogDebug("Evaluation interactions based on mailbox for user '{user}' completed. Processed {mailsCount} email/s", "***", mailboxTraverser.FetchedMessagesCount);
-                _logger.LogTrace("Evaluation interactions based on mailbox for user '{user}' completed. Processed {mailsCount} email/s", userEmail, mailboxTraverser.FetchedMessagesCount);
-
-                return new SingleTaskResult(interactionsCount);
-            }
-            catch (TooManyMailsPerUserException tmmpuex)
-            {
-                await _statusLogger.LogWarningAsync($"Skipping mailbox '***' too many messages", stoppingToken);
-                _logger.LogWarning("Skipping mailbox '{email}' too many messages", "***");
-                _logger.LogTrace("Skipping mailbox '{email}' too many messages", tmmpuex.Email);
-                return SingleTaskResult.Empty;
-            }
-            catch (GoogleApiException gaex) when (IsMailServiceNotEnabledException(gaex))
-            {
-                _logger.LogWarning("Skipping mailbox '{email}' gmail service not enabled", "***");
-                _logger.LogTrace("Skipping mailbox '{email}' gmail service not enabled", userEmail);
-                return SingleTaskResult.Empty;
-            }
+            await statusLogger.LogWarningAsync($"Skipping mailbox '***' too many messages", stoppingToken);
+            _logger.LogWarning("Skipping mailbox '{email}' too many messages", "***");
+            return SingleTaskResult.Empty;
         }
-
-        private int CalculateMaxMessagesCount(TimeRange timeRange)
+        catch (GoogleApiException gaex) when (IsMailServiceNotEnabledException(gaex))
         {
-            var maxMessagesCount = (int)(_config.MaxMessagesPerUserDaily * timeRange.Duration.TotalMinutes / MinutesInDay);
-
-            var result = maxMessagesCount > 0 ? maxMessagesCount : 1;
-
-            _logger.LogDebug("For synchronization period {timerange} max allowed messages is {count}", timeRange, result);
-
-            return result;
+            _logger.LogWarning("Skipping mailbox '{email}' gmail service not enabled", "***");
+            return SingleTaskResult.Empty;
         }
+    }
 
-        private async Task<GmailService> InitializeGmailServiceAsync(string userEmail, CancellationToken stoppingToken)
+    private int CalculateMaxMessagesCount(TimeRange timeRange)
+    {
+        var maxMessagesCount = (int)(_config.MaxMessagesPerUserDaily * timeRange.Duration.TotalMinutes / MinutesInDay);
+
+        var result = maxMessagesCount > 0 ? maxMessagesCount : 1;
+
+        _logger.LogDebug("For synchronization period {timerange} max allowed messages is {count}", timeRange, result);
+
+        return result;
+    }
+
+    private async Task<GmailService> InitializeGmailServiceAsync(string userEmail, CancellationToken stoppingToken)
+    {
+        var credentials = await credentialsProvider.ImpersonificateAsync(userEmail, stoppingToken);
+
+        return new GmailService(new BaseClientService.Initializer
         {
-            var credentials = await _credentialsProvider.ImpersonificateAsync(userEmail, stoppingToken);
+            HttpClientInitializer = credentials,
+            ApplicationName = _config.ApplicationName
+        });
+    }
 
-            return new GmailService(new BaseClientService.Initializer
-            {
-                HttpClientInitializer = credentials,
-                ApplicationName = _config.ApplicationName
-            });
-        }
-
-        private static bool IsMailServiceNotEnabledException(GoogleApiException exception)
-        {
-            const string exceptionDomain = "global";
-            const string exceptionReason = "failedPrecondition";
-            const string exceptionMessage = "Mail service not enabled";
-            return exception.Error.Errors.Any(x => x.Domain == exceptionDomain && x.Reason == exceptionReason && x.Message == exceptionMessage);
-        }
+    private static bool IsMailServiceNotEnabledException(GoogleApiException exception)
+    {
+        const string exceptionDomain = "global";
+        const string exceptionReason = "failedPrecondition";
+        const string exceptionMessage = "Mail service not enabled";
+        return exception.Error.Errors.Any(x => x.Domain == exceptionDomain && x.Reason == exceptionReason && x.Message == exceptionMessage);
     }
 }
